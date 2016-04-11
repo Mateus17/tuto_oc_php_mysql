0add3f8b76a10d073f21edecc78e7096c119023c
Mathieu Rodrigues <mathieu.rodrigues17@gmail.com>
2016-04-11 22:40:09 +0200

    URL, array, help

--- tests/index.php
+++ tests/index.php
@@ -1,30 +1,34 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8" />
-        <title>Mon super site</title>
-    </head>
- 
-    <body>
- 
-    <?php include("entete.php"); ?>
-    
-    <?php include("menus.php"); ?>
-    
-    <!-- Le corps -->
-    
-    <div id="corps">
-        <h1>Mon super site</h1>
-        
-        <p>
-            Bienvenue sur mon super site !<br />
-            Vous allez adorer ici, c'est un site génial qui va parler de... euh... Je cherche encore un peu le thème de mon site. :-D
-        </p>
-    </div>
-    
-    <!-- Le pied de page -->
-    
-    <?php include("pied_de_page.php"); ?>
-    
-    </body>
+
+<head>
+   <meta charset="utf-8" />
+   <title>Mon super site</title>
+</head>
+
+<body>
+
+   <?php include("entete.php"); ?>
+
+      <?php include("menus.php"); ?>
+
+         <!-- Le corps -->
+
+         <div id="corps">
+            <h1>Mon super site</h1>
+
+            <p>
+               Bienvenue sur mon super site !
+               <br /> Vous allez adorer ici, c'est un site génial qui va parler de... euh... Je cherche encore un peu le thème de mon site. :-D
+            </p>
+
+            <a href="bonjour.php?nom=Dupont&amp;prenom=Jean">Dis-moi bonjour !</a>
+         </div>
+
+         <!-- Le pied de page -->
+
+         <?php include("pied_de_page.php"); ?>
+
+</body>
+
 </html>
